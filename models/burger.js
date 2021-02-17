59b71970188967544eedab6093c666991e3e175f
apemint <escobar.andres.alfonso@gmail.com>
2021-02-16 20:30:22 -0500

    server, models and html/css seem to be working

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,31 @@
 //import orm.js into burger.js
+
+const orm = require("../config/orm.js");
 //create code that call the orm functions using burger specific input for the orm
+const burger = {
+    all: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    insert: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+
+    update: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+
+    delete: function (condition, cb) {
+        orm.deleteOne("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
+}
 //export at end of burger.js
+module.exports = burger;
