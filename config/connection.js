49423c2990c0d306b9a264f5dc4c3ffd2d78a1af
apemint <escobar.andres.alfonso@gmail.com>
2021-02-17 11:20:25 -0500

    tring to change connection see if it works on deploy

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@
 
 let mysql = require("mysql");
 
-var connection;
+const connection;
 
-if (process.env.JAWSBD_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
@@ -17,6 +17,12 @@ if (process.env.JAWSBD_URL) {
 
 }
 
-connection.connect();
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 
 module.exports = connection;
